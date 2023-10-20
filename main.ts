c02e12cb5f105d54fc6342a61980608e48336ab6
Karl Edenstål <karl.edenstal@gmail.com>
2023-10-20 15:29:30 +0200

    change default port

--- main.ts
+++ main.ts
@@ -1,5 +1,5 @@
 import { Elysia } from 'elysia';
 
-const app = new Elysia().get('/', () => ({ hello: 'Bun👋' })).listen(8080);
+const app = new Elysia().get('/', () => ({ hello: 'Bun👋' })).listen(1234);
 
 console.log(`Listening on http://localhost:${app.server!.port}`);
